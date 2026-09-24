132d833532e9cad399fbf0bbe0e618a1be15b17e
Yosef <mercytv932@gmail.com>
2026-09-24 19:13:04 -0400

    small debugging and testing done

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongooose.Schema({
+const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true, min: 0.01 },
